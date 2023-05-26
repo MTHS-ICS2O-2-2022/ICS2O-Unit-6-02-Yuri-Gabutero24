9a128870e3f1ef73be921070a8c1f7bdb272ff82
Yuri-Gabutero24 <124618894+Yuri-Gabutero24@users.noreply.github.com>
2023-05-26 17:02:29 +0000

    cookie clicker

--- js/script.js
+++ js/script.js
@@ -1,5 +1,19 @@
 // Copyright (c) 2020 Mr. Coxall All rights reserved
 //
 // Created by: Mr. Coxall
-// Created on: Sep 2020
+// Created on: May 2023
 // This file contains the JS functions for index.html
+
+"use strict"
+
+function myButtonClicked() {
+  //process
+  if (localStorage.cookies) {
+    localStorage.cookies = Number(localStorage.cookies) + 1
+  } else {
+    localStorage.cookies = 1
+  }
+
+  //output
+  document.getElementById('score').innerHTML = "You have " + localStorage.cookies + " cookies!"
+}
